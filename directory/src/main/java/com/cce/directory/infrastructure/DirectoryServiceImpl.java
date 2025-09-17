7477a774a77605c04b120950027167f71fb4cee4
Sebastian Bran <sebastianbranzapata@gmail.com>
2025-09-17 11:57:20 -0500

    Add cache for isUserEntity validation

--- directory/src/main/java/com/cce/directory/infrastructure/DirectoryServiceImpl.java
+++ directory/src/main/java/com/cce/directory/infrastructure/DirectoryServiceImpl.java
@@ -41,6 +41,7 @@ public class DirectoryServiceImpl implements DirectoryService {
     }
 
     @Override
+    @Cacheable(value = "userEntityValidation", key = "#phoneNumber + '_' + #entityId")
     public boolean isUserInEntity(String phoneNumber, String entityId) {
         logger.info("Validating if user with phone number {} exists in entity with id {}", phoneNumber, entityId);
         return this.directoryRepository.existsByPhoneNumberAndEntityId(phoneNumber, entityId);
